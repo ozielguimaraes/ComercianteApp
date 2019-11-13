6066afbdeb773a5f39c5eafaebe92975de6069c7
Pedro Henrique <pedrohqesilva@gmail.com>
2019-11-12 21:09:37 -0300

    Inicio do projeto

---
+++ Presentation/Forms/Pages/Menus/Menu.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace Forms.Pages.Menus
+{
+    public partial class Menu : Form
+    {
+        public Menu()
+        {
+            InitializeComponent();
+        }
+    }
+}

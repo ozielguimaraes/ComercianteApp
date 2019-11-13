5ad20dc8ad416fcaab8dd1a567652916be8380ca
Pedro Henrique <pedrohqesilva@gmail.com>
2019-11-12 21:30:43 -0300

    Base entity

---
+++ src/Domain/AggregatesModels/Base/EntityBase.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Domain.AggregatesModels.Base
+{
+    public class EntityBase
+    {
+        public long Id { get; protected set; }
+        public Guid Guid { get; protected set; }
+    }
+}
---
+++ src/Domain/AggregatesModels/Base/Metadata.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Domain.AggregatesModels.Base
+{
+    public class Metadata
+    {
+        public DateTime Criacao { get; private set; } = DateTime.Now;
+        public DateTime? Alteracao { get; private set; }
+        public DateTime? Exclusao { get; private set; }
+
+        public bool Ativo => Exclusao.HasValue || (Criacao >= DateTime.Now && DateTime.Now <= Exclusao);
+
+        public Metadata()
+        {
+        }
+    }
+}

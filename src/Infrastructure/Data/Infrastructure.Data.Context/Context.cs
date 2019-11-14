fae51ee36c087047d031e9c58bb41cfed81a2f92
Pedro Henrique <pedrohqesilva@gmail.com>
2019-11-13 23:41:20 -0300

    Inclusão do dbContext e ajuste nas libraries para apontar para o Standard 2.1

---
+++ src/Infrastructure/Data/Infrastructure.Data.Context/Context.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data.Context
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasDefaultSchema("ComercianteDB");
+            ConfigureTypes(modelBuilder);
+        }
+
+        private void ConfigureTypes(ModelBuilder modelBuilder)
+        {
+            foreach (var property in modelBuilder.Model.GetEntityTypes()
+                .SelectMany(t => t.GetProperties())
+                .Where(p => p.ClrType == typeof(string)))
+            {
+                property.SetIsUnicode(true);
+            }
+        }
+    }
+}

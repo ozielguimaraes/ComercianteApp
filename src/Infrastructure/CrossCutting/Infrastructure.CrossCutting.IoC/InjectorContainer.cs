5e9e55369ebcc18d2d9923b7b4e1e994d30ef5bc
Pedro Henrique <pedrohqesilva@gmail.com>
2019-11-14 23:26:57 -0300

    Ajuste injeção de dependência

--- src/Infrastructure/CrossCutting/Infrastructure.CrossCutting.IoC/InjectorContainer.cs
+++ src/Infrastructure/CrossCutting/Infrastructure.CrossCutting.IoC/InjectorContainer.cs
@@ -21,70 +21,96 @@ namespace Infrastructure.CrossCutting.IoC
             // services.AddScoped<IEventStoreRepository, EventStoreRepository>();
             // services.AddScoped<INotificationHandler<DomainNotification>, DomainNotificationHandler>();
 
-            if (!string.IsNullOrEmpty(connectionString))
-            {
-                services.AddScoped((serviceProvider) =>
-                {
-                    var dbConnection = new SqlConnection(connectionString);
-                    dbConnection.Open();
-                    return dbConnection;
-                });
-
-                services.AddScoped<DbTransaction>((serviceProvider) =>
-                {
-                    var dbConnection = serviceProvider.GetService<SqlConnection>();
-                    return dbConnection.BeginTransaction(IsolationLevel.ReadCommitted);
-                });
-
-                services.AddScoped((serviceProvider) =>
-                {
-                    var dbConnection = serviceProvider.GetService<SqlConnection>();
-
-                    var dbContext = new DbContextOptionsBuilder()
-                        .UseSqlServer(dbConnection, x =>
-                        {
-                            x.MigrationsHistoryTable("Migrations");
-                            x.MigrationsAssembly("Infrastructure.Data.Migrations");
-                        })
-                        .UseLazyLoadingProxies()
-                        .EnableSensitiveDataLogging();
-
-                    AddContextLog(dbContext);
-
-                    return dbContext.Options;
-                });
-            }
+            ConfigureDbOptions(services, connectionString);
 
             // InjectContext<EventStoreContext>(services, connectionString);
+        }
+
+        private static void ConfigureDbOptions(IServiceCollection services, string connectionString)
+        {
+            if (!string.IsNullOrEmpty(connectionString))
+            {
+                AddConnection(services, connectionString);
+                AddTransaction(services);
+                AddDbOptions(services);
+            }
+        }
+
+        private static void AddConnection(IServiceCollection services, string connectionString)
+        {
+            services.AddScoped((serviceProvider) =>
+            {
+                var dbConnection = new SqlConnection(connectionString);
+                dbConnection.Open();
+                return dbConnection;
+            });
+        }
+
+        private static void AddTransaction(IServiceCollection services)
+        {
+            services.AddScoped<DbTransaction>((serviceProvider) =>
+            {
+                var dbConnection = serviceProvider.GetService<SqlConnection>();
+                return dbConnection.BeginTransaction(IsolationLevel.ReadCommitted);
+            });
+        }
+
+        private static void AddDbOptions(IServiceCollection services)
+        {
+            services.AddScoped((serviceProvider) =>
+            {
+                var dbConnection = serviceProvider.GetService<SqlConnection>();
+
+                var dbContext = new DbContextOptionsBuilder()
+                    .UseSqlServer(dbConnection, x =>
+                    {
+                        x.MigrationsHistoryTable("Migrations");
+                        x.MigrationsAssembly("Infrastructure.Data.Migrations");
+                    })
+                    .UseLazyLoadingProxies()
+                    .EnableSensitiveDataLogging();
+
+                AddContextLog(dbContext);
+                return dbContext.Options;
+            });
         }
 
         public static void InjectContext(IServiceCollection services, string connectionString)
         {
             if (string.IsNullOrWhiteSpace(connectionString))
             {
-                var serviceProvider = new ServiceCollection()
-                    .AddEntityFrameworkInMemoryDatabase()
-                    .AddEntityFrameworkProxies()
-                    .BuildServiceProvider();
-
-                services.AddDbContext<ComercianteContext>(options => options
-                    .UseInternalServiceProvider(serviceProvider)
-                    .UseInMemoryDatabase("ComercianteDb")
-                    .UseLazyLoadingProxies()
-                    .EnableSensitiveDataLogging());
+                UseInMemoryDatabase(services);
+                return;
             }
-            else
+
+            UseSqlDatabase(services);
+        }
+
+        private static void UseInMemoryDatabase(IServiceCollection services)
+        {
+            var serviceProvider = new ServiceCollection()
+                .AddEntityFrameworkInMemoryDatabase()
+                .AddEntityFrameworkProxies()
+                .BuildServiceProvider();
+
+            services.AddDbContext<ComercianteContext>(options => options
+                .UseInternalServiceProvider(serviceProvider)
+                .UseInMemoryDatabase("ComercianteDb")
+                .UseLazyLoadingProxies()
+                .EnableSensitiveDataLogging());
+        }
+
+        private static void UseSqlDatabase(IServiceCollection services)
+        {
+            services.AddScoped((serviceProvider) =>
             {
-                services.AddScoped((serviceProvider) =>
-                {
-                    var transaction = serviceProvider.GetService<DbTransaction>();
-                    var options = serviceProvider.GetService<DbContextOptions>();
+                var transaction = serviceProvider.GetService<DbTransaction>();
+                var options = serviceProvider.GetService<DbContextOptions>();
 
-                    var context = (ComercianteContext)Activator.CreateInstance(typeof(ComercianteContext), options);
-                    context.Database.UseTransaction(transaction);
-                    return context;
-                });
-            }
+                var context = (ComercianteContext)Activator.CreateInstance(typeof(ComercianteContext), options);
+                context.Database.UseTransaction(transaction);
+                return context;
+            });
         }
 
         private static ILoggerFactory GetLoggerFactory()
